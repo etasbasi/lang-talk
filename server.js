6801ea7f589011332f188c1ba67e927d4bc9cb81
Enes <enestasbasi@outlook.com>
2018-12-25 01:56:42 -0500

    served static content

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
 
@@ -38,5 +39,13 @@ app.get("/test", (req, res) => res.json({ msg: "/ Works" }));
 app.use("/api/users/", users);
 app.use("/api/profile/", profile);
 app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(___dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log("Server is up on port " + port));
